832fbfbd1e33c845c2065b280a2ec687e6bb35ca
asanchez <asanchez@arrowheadgrp.com>
2019-11-11 12:33:10 -0800

    Initial commit of Lesson 7 material

---
+++ Lesson7Database/src/TestDB.java
@@ -0,0 +1,64 @@
+import java.nio.file.*;
+import java.sql.*;
+import java.io.*;
+import java.util.*;
+
+public class TestDB {
+	public static void main(String[] args) throws IOException {
+		try {
+			runTest();
+		}
+		catch (SQLException ex) {
+			for (Throwable t : ex) {
+				t.printStackTrace();
+			}
+		}
+	}
+	
+	/**
+	 * 
+	 * 
+	 * @throws SQLException
+	 * @throws IOException
+	 */
+	
+	public static void runTest() throws SQLException, IOException {
+		
+		try (Connection conn = getConnection();
+				Statement stat = conn.createStatement()) {
+			stat.executeUpdate("CREATE TABLE Greetings (Message CHAR(20))");
+			stat.executeUpdate("INSERT INTO Greetings VALUES('Hello, World')");
+			
+			
+			try (ResultSet result = stat.executeQuery("SELECT * FROM Greetings")){
+				if (result.next()) {
+					System.out.println(result.getString(1));
+				}
+				stat.executeUpdate("DROP TABLE Greetings");
+			}
+		}
+	}
+	
+	/**
+	 * This guy connects to database.
+	 * @return
+	 * @throws SQLException
+	 * @throws IOException
+	 */
+	public static Connection getConnection() throws SQLException, IOException {
+		
+		Properties props = new Properties();
+		// 
+		try (InputStream in = Files.newInputStream(Paths.get("database.properties"))) {
+			// Loads all properties from input stream.
+			props.load(in);
+		}
+		String drivers = props.getProperty("jdbc.drivers");
+		if (drivers != null) System.setProperty("jdbc.drivers", drivers);
+		String url = props.getProperty("jdbc.url");
+		String username = props.getProperty("jdbc.username");
+		String password = props.getProperty("jdbc.password");
+		
+		return DriverManager.getConnection(url, username, password);
+	}
+}
